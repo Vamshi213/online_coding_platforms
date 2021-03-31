7748faec9e984c01c30f4b258ee73e3443124ce9
Vamshi213 <79149752+Vamshi213@users.noreply.github.com>
2021-03-31 05:09:25 -0500

    Create WOS5.sql

---
+++ hackerrank/SQL/WOS5.sql
@@ -0,0 +1,2 @@
+/*Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.8?
+(select CITY, char_length(CITY) as len_city from STATION order by len_city,city limit 1) union all (select CITY, char_length(CITY) as len_city from STATION order by len_city desc limit 1);
